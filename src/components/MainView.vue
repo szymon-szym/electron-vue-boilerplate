17217e3bceb0d3ae193675b21046fe44b553ebce
szymon-szym <szymon.mich@gmail.com>
2018-10-23 22:43:57 +0200

    logger and lowdb in progress

---
+++ src/components/MainView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mainView">
+    <h1>{{ test }}</h1>
+    <v-btn @click="testLog">button</v-btn>
+  </div>
+</template>
+
+<script>
+const log = require('electron-log')
+export default {
+  name: 'MainView',
+  data() {
+    return {
+      test: 'test'
+    }
+  },
+  methods: {
+    testLog() {
+      log.info('hello log')
+      log.debug('debug msg')
+      log.warn('warn')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
